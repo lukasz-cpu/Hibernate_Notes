051b0ade33e07706255ac8709ba0d50a7de78143
lukasz_p <lukasz.popek@hotmail.com>
2023-07-17 19:06:05 +0200

    add

---
+++ note1/src/main/java/App.java
@@ -0,0 +1,19 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class App {
+
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+
+    public static void main(String[] args) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        em.getTransaction().begin();
+
+
+        em.getTransaction().commit();
+        em.close();
+
+    }
+}
---
+++ note1/src/test/java/com/example/note1/Note1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.note1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Note1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
