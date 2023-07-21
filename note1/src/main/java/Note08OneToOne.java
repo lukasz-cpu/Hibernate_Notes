8479f4f280ceb62f293485e6f7b702166d27abb6
lukasz_p <lukasz.popek@hotmail.com>
2023-07-21 15:14:02 +0200

    Create Note08OneToOne.java

---
+++ note1/src/main/java/Note08OneToOne.java
@@ -0,0 +1,32 @@
+import entity.Product;
+import entity.Review;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
+
+@SpringBootApplication
+public class Note08OneToOne {
+    private static final Logger log = LogManager.getLogger(Note02Read.class);
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("unit");
+
+    public static void main(String[] args) {
+        EntityManager em = entityManagerFactory.createEntityManager();
+
+        em.getTransaction().begin();
+
+
+        
+
+
+        em.getTransaction().commit();
+        em.close();
+    }
+
+
+}
