3e811bab33c263599d035daf9c38f77e25a64ebf
lukasz_p <lukasz.popek@hotmail.com>
2023-07-22 13:16:55 +0200

    Update schema.sql

--- note1/src/main/resources/schema/schema.sql
+++ note1/src/main/resources/schema/schema.sql
@@ -39,3 +39,22 @@ ALTER TABLE `product`
 ADD CONSTRAINT fk_category_id
 FOREIGN KEY (`category_id`)
 REFERENCES category (`id`)
+
+CREATE TABLE `attribute` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NULL,
+  `value` VARCHAR(800) NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `product_attribute` (
+`product_id` BIGINT NOT NULL,
+`attribute_id` BIGINT NOT NULL,
+PRIMARY KEY (`product_id`, `attribute_id`),
+ CONSTRAINT `fk_product_attribute_product_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `product` (`id`),
+ CONSTRAINT `fk_product_attribute_attribute_id`
+    FOREIGN KEY (`attribute_id`)
+    REFERENCES `attribute` (`id`)
+);
