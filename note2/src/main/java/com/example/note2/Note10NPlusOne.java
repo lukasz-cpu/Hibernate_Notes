2d41a5a53416d69893a7f213bcfe029b44887246
lukasz_p <lukasz.popek@hotmail.com>
2023-08-07 16:57:24 +0200

    Update Note10NPlusOne.java

--- note2/src/main/java/com/example/note2/Note10NPlusOne.java
+++ note2/src/main/java/com/example/note2/Note10NPlusOne.java
@@ -1,13 +1,10 @@
 package com.example.note2;
 
 import com.example.note2.entity.Order;
-import jakarta.persistence.EntityGraph;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -25,10 +22,13 @@ public class Note10NPlusOne {
 
 //    List<Order> orderList = em.createQuery("select o from Order o inner join fetch o.orderRows", Order.class).getResultList();
 
-    EntityGraph<?> entityGraph = em.getEntityGraph("order-and-rows");
+//    EntityGraph<?> entityGraph = em.getEntityGraph("order-and-rows");
+//
+//    List<Order> orderList = em.createQuery("select o from Order o", Order.class)
+//        .setHint("jakarta.persistence.fetchgraph", entityGraph).getResultList();
 
-    List<Order> orderList = em.createQuery("select o from Order o", Order.class)
-        .setHint("jakarta.persistence.fetchgraph", entityGraph).getResultList();
+    List<Order> orderList = em.createQuery("select o from Order o",
+        Order.class).getResultList();
 
     for (Order order : orderList) {
       log.info(order.toString());
