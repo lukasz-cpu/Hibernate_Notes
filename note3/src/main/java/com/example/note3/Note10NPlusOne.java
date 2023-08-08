d443899842490576f1ecaf627e30bd60ca88aee8
lukasz_p <lukasz.popek@hotmail.com>
2023-08-08 16:08:57 +0200

    batch is not working

--- note3/src/main/java/com/example/note3/Note10NPlusOne.java
+++ note3/src/main/java/com/example/note3/Note10NPlusOne.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Persistence;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import org.hibernate.Session;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -18,17 +19,18 @@ public class Note10NPlusOne {
 
   public static void main(String[] args) {
     EntityManager em = entityManagerFactory.createEntityManager();
+    em.unwrap(Session.class).setJdbcBatchSize(5);
     em.getTransaction().begin();
 
-    Long lastId = em.createQuery("select max(r.id) from BatchReview r", Long.class)
-        .getSingleResult();
+    Long lastId = em.createQuery("select max(r.id) from BatchReview r", Long.class).getSingleResult();
 
-    for (int i = 1; i <= 30; i++) {
+    for (long i = 1; i <= 30; i++) {
+      if (i % 5 == 0) {
+        em.flush();
+        em.clear();
+      }
       em.persist(new BatchReview(lastId + i, "Treść", 5, 1L));
     }
-
-
-    log.info("JESTEM TUTAJ");
 
     em.getTransaction().commit();
     em.close();
