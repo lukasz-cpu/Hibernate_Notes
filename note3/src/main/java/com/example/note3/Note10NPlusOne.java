27cd3dd60a37c5226c72000806a90eb278ed2aae
lukasz_p <lukasz.popek@hotmail.com>
2023-08-08 16:49:13 +0200

    Update Note10NPlusOne.java

--- note3/src/main/java/com/example/note3/Note10NPlusOne.java
+++ note3/src/main/java/com/example/note3/Note10NPlusOne.java
@@ -19,11 +19,16 @@ public class Note10NPlusOne {
 
   public static void main(String[] args) {
     EntityManager em = entityManagerFactory.createEntityManager();
+//    em.unwrap(Session.class).setJdbcBatchSize(10);
     em.getTransaction().begin();
 
     Long lastId = em.createQuery("select max(r.id) from BatchReview r", Long.class).getSingleResult();
 
-    for (long i = 1; i <= 60; i++) {
+    for (long i = 1; i <= 30; i++) {
+//      if (i % 5 == 0) {
+//        em.flush();
+//        em.clear();
+//      }
       em.persist(new BatchReview(lastId + i, "Treść", 5, 1L));
     }
 
