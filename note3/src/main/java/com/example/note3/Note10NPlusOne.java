7ea04bb9b21bac99080cb9dd0c287b16ea7892b9
lukasz_p <lukasz.popek@hotmail.com>
2023-08-08 15:39:04 +0200

    not working yet

--- note3/src/main/java/com/example/note3/Note10NPlusOne.java
+++ note3/src/main/java/com/example/note3/Note10NPlusOne.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
@@ -25,6 +26,9 @@ public class Note10NPlusOne {
     for (int i = 1; i <= 30; i++) {
       em.persist(new BatchReview(lastId + i, "Treść", 5, 1L));
     }
+
+
+    log.info("JESTEM TUTAJ");
 
     em.getTransaction().commit();
     em.close();
