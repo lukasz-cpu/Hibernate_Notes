3ca18e6ebb4943aa2b040a09451bb0dd61016106
lukasz_p <lukasz.popek@hotmail.com>
2023-08-10 18:56:02 +0200

    asd

--- note4/src/main/java/com/example/note4/Note02Filtering.java
+++ note4/src/main/java/com/example/note4/Note02Filtering.java
@@ -12,6 +12,7 @@ import jakarta.persistence.criteria.JoinType;
 import jakarta.persistence.criteria.ParameterExpression;
 import jakarta.persistence.criteria.Root;
 import java.math.BigDecimal;
+import java.util.Collection;
 import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -35,20 +36,20 @@ public class Note02Filtering {
     orders.fetch("orderRows").fetch("product");
     ParameterExpression<Long> id1 = criteriaBuilder.parameter(Long.class);
     ParameterExpression<Long> id2 = criteriaBuilder.parameter(Long.class);
+    ParameterExpression<String> lastname = criteriaBuilder.parameter(String.class);
     ParameterExpression<BigDecimal> total = criteriaBuilder.parameter(BigDecimal.class);
+    ParameterExpression<Collection> ids = criteriaBuilder.parameter(Collection.class);
     criteriaQuery.select(customer).distinct(true)
         .where(
-            criteriaBuilder.greaterThan(orders.get("total"), total),
-            criteriaBuilder.and(
-                criteriaBuilder.or(
-//                    criteriaBuilder.equal(customer.get("id"), id1),
-//                    criteriaBuilder.equal(customer.get("id"), id2)
-                    criteriaBuilder.like(customer.get("lastname"), "K%")
-                )));
+            criteriaBuilder.between(orders.get("total"), total,
+                criteriaBuilder.literal(new BigDecimal("70.00"))),
+            criteriaBuilder.isNotNull(customer.get("firstname")),
+            customer.get("id").in(ids));
 
     TypedQuery<Customer> query = em.createQuery(criteriaQuery);
 //    query.setParameter(id1, 1L);
 //    query.setParameter(id2, 1L);
+    query.setParameter(ids, List.of(1L, 2L, 4L));
     query.setParameter(total, new BigDecimal("30.00"));
     List<Customer> resultList = query.getResultList();
     for (Customer result : resultList) {
---
+++ note4/src/main/java/com/example/note4/Note03Aggregating.java
@@ -0,0 +1,60 @@
+package com.example.note4;
+
+import com.example.note4.entity.Customer;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.ParameterExpression;
+import jakarta.persistence.criteria.Root;
+import java.math.BigDecimal;
+import java.util.Collection;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Note03Aggregating {
+
+  private static Logger log = LogManager.getLogger(Note01CriteriaJoin.class);
+  private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(
+      "unit");
+
+  public static void main(String[] args) {
+    EntityManager em = entityManagerFactory.createEntityManager();
+    em.getTransaction().begin();
+
+    CriteriaBuilder criteriaBuilder = em.getCriteriaBuilder();
+    CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+    Root<Customer> customer = criteriaQuery.from(Customer.class);
+    Join<Object, Object> orders = customer.join("orders", JoinType.INNER);
+    Join<Object, Object> orderRows = orders.join("orderRows");
+    Join<Object, Object> product = orderRows.join("product");
+    Join<Object, Object> category = product.join("category");
+
+    criteriaQuery.multiselect(
+            customer.get("id"),
+            customer.get("lastname"),
+            category.get("name"),
+            criteriaBuilder.sum(orderRows.get("price"))
+        )
+        .groupBy(category.get("id"), customer.get("id"))
+        .orderBy(criteriaBuilder.desc(criteriaBuilder.sum(orderRows.get("price"))))
+        .having(criteriaBuilder.greaterThan(criteriaBuilder.sum(orderRows.get("price")), 50));
+
+    TypedQuery<Object[]> query = em.createQuery(criteriaQuery);
+    List<Object[]> resultList = query.getResultList();
+    for (Object[] row : resultList) {
+      log.info(row[0] + ", " + row[1] + ", " + row[2] + ", " + row[3]);
+    }
+
+    em.getTransaction().commit();
+    em.close();
+
+  }
+}
