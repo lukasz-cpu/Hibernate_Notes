e179214a9b795ef29e1804f83537abaea15929f1
lukasz_p <lukasz.popek@hotmail.com>
2023-08-12 11:14:51 +0200

    added new lines of code

--- note5/src/main/java/com/example/note5/Note04Inheriting.java
+++ note5/src/main/java/com/example/note5/Note04Inheriting.java
@@ -1,9 +1,12 @@
 package com.example.note5;
 
 import com.example.note5.entity.Order;
+import com.example.note5.entity.RealProduct;
+import com.example.note5.entity.VirtualProduct;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+import java.time.LocalDateTime;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,7 +20,30 @@ public class Note04Inheriting {
     em.getTransaction().begin();
 
 
+    VirtualProduct virtualProduct = new VirtualProduct();
+    virtualProduct.setName("Virtual");
+    virtualProduct.setDescription("Desc virtual");
+    virtualProduct.setCreated(LocalDateTime.now());
+    virtualProduct.setDownloadable(true);
+    virtualProduct.setFileName("test.txt");
+    virtualProduct.setFilePath("/store");
+    em.persist(virtualProduct);
 
+    RealProduct realProduct = new RealProduct();
+    realProduct.setName("Real");
+    realProduct.setDescription("Desc real");
+    realProduct.setCreated(LocalDateTime.now());
+    realProduct.setWeight(10.5f);
+    realProduct.setWidth(20);
+    realProduct.setLength(10);
+    realProduct.setHeight(30);
+    em.persist(realProduct);
+
+    em.flush();
+    em.clear();
+
+    log.info(em.find(VirtualProduct.class, virtualProduct.getId()));
+    log.info(em.find(RealProduct.class, realProduct.getId()));
 
 
 
---
+++ note5/src/main/java/com/example/note5/entity/BaseProduct.java
@@ -0,0 +1,82 @@
+package com.example.note5.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.DiscriminatorType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "base_product")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "product_type", discriminatorType = DiscriminatorType.STRING)
+public abstract class BaseProduct {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String name;
+  private String description;
+  private LocalDateTime created;
+  @Enumerated(EnumType.STRING)
+  @Column(name = "product_type", insertable = false, updatable = false)
+  private ProductType productType;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public LocalDateTime getCreated() {
+    return created;
+  }
+
+  public void setCreated(LocalDateTime created) {
+    this.created = created;
+  }
+
+  public ProductType getProductType() {
+    return productType;
+  }
+
+  public void setProductType(ProductType productType) {
+    this.productType = productType;
+  }
+
+  @Override
+  public String toString() {
+    return "BaseProduct{" +
+        "id=" + id +
+        ", name='" + name + '\'' +
+        ", description='" + description + '\'' +
+        ", created=" + created +
+        ", productType=" + productType +
+        '}';
+  }
+}
