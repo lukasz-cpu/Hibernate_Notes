78990220213d986649837299c6ac487ee24873ee
lukasz_p <lukasz.popek@hotmail.com>
2023-08-16 11:52:29 +0200

    added new lines of code

--- note8/src/main/java/com/example/note8/Note02LockingPesimistic.java
+++ note8/src/main/java/com/example/note8/Note02LockingPesimistic.java
@@ -3,6 +3,7 @@ package com.example.note8;
 import com.example.note8.entity.Order;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.LockModeType;
 import jakarta.persistence.Persistence;
 import java.math.BigDecimal;
 import org.apache.logging.log4j.LogManager;
@@ -10,31 +11,26 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Note01Locking {
+public class Note02LockingPesimistic {
 
-  private static Logger log = LogManager.getLogger(Note01Locking.class);
+  private static Logger log = LogManager.getLogger(Note02LockingPesimistic.class);
   private static EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory(
       "unit");
 
   public static void main(String[] args) {
     EntityManager em = entityManagerFactory.createEntityManager();
     em.getTransaction().begin();
+//        Order order = em.find(Order.class, 3L, LockModeType.PESSIMISTIC_READ);
+//        logger.info(order);
 
-    EntityManager em2 = entityManagerFactory.createEntityManager();
-    em2.getTransaction().begin();
+    Order singleResult = em.createQuery("select o from Order o where o.id=:id", Order.class)
+        .setParameter("id", 3L)
+        .setLockMode(LockModeType.PESSIMISTIC_WRITE)
+        .getSingleResult();
+    log.info(singleResult);
 
-    Order order = em.find(Order.class, 3L);
-    Order order2 = em2.find(Order.class, 3L);
-
-    order.setTotal(new BigDecimal("22.35"));
-    log.info(order);
     em.getTransaction().commit();
-    em.close();
-
-    order2.setTotal(new BigDecimal("22.44"));
-    log.info(order2);
-    em2.getTransaction().commit();
-    em2.close();
+    em.clear();
   }
 
 }
